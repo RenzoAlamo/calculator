c3a4e3dfe8113a4299592ec6d7fbfa69791b07c7
Renzo Alamo <renzoalamo@hotmail.com>
2022-01-17 01:03:39 -0500

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,63 @@
+import { CSSProperties, MouseEventHandler, useState } from "react";
 import "./App.css";
 
-const buttons: { name: string; value: number | string }[] = [
-	{ name: "7", value: 7 },
-	{ name: "8", value: 8 },
-	{ name: "9", value: 9 },
-	{ name: "×", value: "*" },
-	{ name: "4", value: 4 },
-	{ name: "5", value: 5 },
-	{ name: "6", value: 6 },
-	{ name: "-", value: "-" },
-	{ name: "1", value: 1 },
-	{ name: "2", value: 2 },
-	{ name: "3", value: 3 },
-	{ name: "+", value: "+" },
-	{ name: " ", value: 1 },
-	{ name: "0", value: 0 },
-	{ name: ".", value: "." },
-	{ name: "=", value: "=" },
-];
-
-const App = () => (
-	<>
-		<div className="calculator">
-			<div className="display"></div>
-			<div className="buttons">
-				{buttons.map((button, index) => (
-					<button key={index} disabled={index === 12}>
-						{button.name}
-					</button>
-				))}
+const App = () => {
+	const [display, setDisplay] = useState("0");
+	const buttons: { text: string; value: number | string; styles?: CSSProperties }[] = [
+		{ text: "C", value: "0", styles: { gridColumn: "span 2" } },
+		{ text: "←", value: -1 },
+		{ text: "÷", value: "/" },
+		{ text: "7", value: 7 },
+		{ text: "8", value: 8 },
+		{ text: "9", value: 9 },
+		{ text: "×", value: "*" },
+		{ text: "4", value: 4 },
+		{ text: "5", value: 5 },
+		{ text: "6", value: 6 },
+		{ text: "−", value: "-" },
+		{ text: "1", value: 1 },
+		{ text: "2", value: 2 },
+		{ text: "3", value: 3 },
+		{ text: "+", value: "+" },
+		{ text: "0", value: 0 },
+		{ text: ".", value: "." },
+		{ text: "=", value: "=", styles: { gridColumn: "span 2" } },
+	];
+	const handler: (buttonText: string) => MouseEventHandler<HTMLButtonElement> = buttonText => _ => {
+		const split = display.split(/[\+|\−|\×|\÷]/);
+		if ([display.slice(-1), buttonText].every(string => /[\+|\−|\×|\÷]/.test(string))) return;
+		if (buttonText === "←") return setDisplay(display => display.slice(0, (buttons.find(({ text }) => text === buttonText) as typeof buttons[number]).value as number));
+		if (buttonText === "C") return setDisplay((buttons.find(({ text }) => text === buttonText) as typeof buttons[number]).value as string);
+		if (buttonText === "=") {
+			if (split[split.length - 1].slice(-1) === ".") return;
+			if (!/[\+|\−|\×|\÷]/.test(display.slice(-1))) return setDisplay(display => String(eval(display.replace(/[\−|\×|\÷]/g, sign => (buttons.find(({ text }) => text === sign) as typeof buttons[number]).value as string))));
+		}
+		if (buttonText === ".") {
+			if (!split[split.length - 1]) return setDisplay(display => display.concat("0", buttonText));
+			if (split[split.length - 1].includes(buttonText)) return;
+		}
+		if (split[split.length - 1] === "0") {
+			if (buttonText === "0") return;
+			if (buttonText !== ".") return setDisplay(display => display.slice(0, -1).concat(buttonText));
+		}
+		if (split[split.length - 1].slice(-1) === "." && /[\+|\−|\×|\÷]/.test(buttonText)) return;
+		setDisplay(display => display.concat(String(buttonText)));
+	};
+	return (
+		<>
+			<div className={"calculator"}>
+				<div className={"display"}>
+					<span>{display}</span>
+				</div>
+				<div className={"buttons"}>
+					{buttons.map(({ text, styles }, index) => (
+						<button key={index} onClick={handler(text)} style={styles}>
+							{text}
+						</button>
+					))}
+				</div>
 			</div>
-		</div>
-	</>
-);
-
+		</>
+	);
+};
 export default App;
